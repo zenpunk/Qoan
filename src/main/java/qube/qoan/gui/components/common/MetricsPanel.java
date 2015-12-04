ab5346163f26ba91184477bae9ca35cd8a2aa4e4
rainbird <tara@chello.at>
2015-12-04 21:23:22 +0100

    metrics-panel in Qoan

---
+++ src/main/java/qube/qoan/gui/components/common/MetricsPanel.java
@@ -0,0 +1,48 @@
+package qube.qoan.gui.components.common;
+
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.VerticalLayout;
+import qube.qai.data.MetricTyped;
+import qube.qai.data.Metrics;
+
+/**
+ * Created by rainbird on 12/4/15.
+ */
+public class MetricsPanel extends Panel {
+
+    /**
+     * displays the contents of MetricTyped data it is given
+     */
+    public MetricsPanel(String name, MetricTyped metricTyped) {
+        super();
+
+        initialize(name, metricTyped);
+
+    }
+
+    private void initialize(String name, MetricTyped metricTyped) {
+
+        VerticalLayout layout = new VerticalLayout();
+
+        Label nameLabel = new Label(name);
+        layout.addComponent(nameLabel);
+
+        Metrics metrics = metricTyped.buildMetrics();
+        for (String p : metrics.getNames()) {
+            HorizontalLayout rowLayout = new HorizontalLayout();
+            String pString = p + ": ";
+            Label pLabel = new Label(pString);
+            rowLayout.addComponent(pLabel);
+            String valueString = metrics.getValue(p).toString();
+            Label valueLabel = new Label(valueString);
+            rowLayout.addComponent(valueLabel);
+            layout.addComponent(rowLayout);
+        }
+
+        setContent(layout);
+    }
+
+
+}
