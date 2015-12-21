74032a0a5b38559aa012de4e2d829c4f5b2d2998
rainbird <tara@chello.at>
2015-12-21 07:05:44 +0100

    milestone really...
    from now on we are to work with hazelcast instances

--- src/main/java/qube/qoan/services/QoanModule.java
+++ src/main/java/qube/qoan/services/QoanModule.java
@@ -1,16 +1,39 @@
 package qube.qoan.services;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
 
 /**
  * Created by rainbird on 11/2/15.
  */
 public class QoanModule extends AbstractModule {
 
+    // for the time being we leave it at that
+    private static String QAI_NODE_NAME = "QaiNode";
+    private HazelcastInstance hazelcastInstance;
+
     @Override
     protected void configure() {
-
+        // for the moment being there is not much here to configure really
         //bind(ProcedureSource.class).to(ProcedureSourceService.class);
+    }
 
+    @Provides
+    @Singleton
+    HazelcastInstance provideHazelcastInstance() {
+        if (hazelcastInstance != null) {
+            return hazelcastInstance;
+        }
+
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.setInstanceName(QAI_NODE_NAME);
+        clientConfig.getNetworkConfig().addAddress("127.0.0.1:5701");
+        hazelcastInstance = HazelcastClient.newHazelcastClient(clientConfig);
+
+        return hazelcastInstance;
     }
 }
