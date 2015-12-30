1d276c52c076c62025737176a15d3aaac503f1b1
rainbird <tara@chello.at>
2015-12-30 13:49:06 +0100

    making the whole thing java 1.7 compatible

--- src/main/java/qube/qoan/services/ProcedureCache.java
+++ src/main/java/qube/qoan/services/ProcedureCache.java
@@ -19,7 +19,7 @@ public class ProcedureCache implements ProcedureSourceInterface {
     private Map<String,Procedure> procedureMap;
 
     public ProcedureCache() {
-        procedureMap = new TreeMap<>();
+        procedureMap = new TreeMap<String,Procedure>();
     }
 
     public void cacheProcedure(Procedure procedure) {
@@ -29,39 +29,32 @@ public class ProcedureCache implements ProcedureSourceInterface {
         procedure.accept(treeBuilder, null);
     }
 
-    @Override
     public Procedure getProcedureWithName(String name) {
         return procedureMap.get(name);
     }
 
-    @Override
     public String[] getProcedureNames() {
         return new String[0];
     }
 
     class ProcedureUUIDCollector implements DataVisitor {
 
-        @Override
         public Object visit(StockEntity visitee, Object data) {
             return data;
         }
 
-        @Override
         public Object visit(StockQuote visitee, Object data) {
             return data;
         }
 
-        @Override
         public Object visit(WikiArticle visitee, Object data) {
             return data;
         }
 
-        @Override
         public Object visit(Network visitee, Object data) {
             return data;
         }
 
-        @Override
         public Object visit(Procedure visitee, Object parent) {
             if (!procedureMap.containsKey(visitee.getUuid())) {
                 procedureMap.put(visitee.getUuid(), visitee);
