de2952eb5b7521d5b6391145cf23ce21c2ba7e3a
rainbird <tara@chello.at>
2016-01-20 15:18:10 +0100

    other changes
    in-between state

---
+++ src/main/java/qube/qoan/gui/components/workspace/document/DocumentMenu.java
@@ -0,0 +1,14 @@
+package qube.qoan.gui.components.workspace.document;
+
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * Created by rainbird on 1/16/16.
+ */
+public class DocumentMenu extends Panel {
+
+    /**
+     * this is for adding and managing pdf-directories in the whole
+     */
+}
